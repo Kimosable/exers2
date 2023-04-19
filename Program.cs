cb9e1c608b07e0e6ee2f1e86325e5699c6dea053
Kimosable <129333154+Kimosable@users.noreply.github.com>
2023-04-19 22:37:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число a: ");
+int i = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int j = int.Parse(Console.ReadLine());
+if (i > j) {
+    Console.WriteLine(i);
+}
+else{
+    Console.WriteLine(j);
+}
